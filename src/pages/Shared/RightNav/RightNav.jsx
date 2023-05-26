d35982b00335c4d4e356e89f2e404e380e2f812c
Shamim raza <shamimraza411@gmail.com>
2023-05-27 00:14:51 +0600

    second commit

--- src/pages/Shared/RightNav/RightNav.jsx
+++ src/pages/Shared/RightNav/RightNav.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import { FaGofore, FaGithub, FaFacebook , FaTwitter, FaInstagram} from 'react-icons/fa';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Qzon from '../Qzone/Qzon';
+import bg from "../../../assets/bg.png"
 
 const RightNav = () => {
     return (
         <div>
-            <h2>right nav</h2>
+            <h4>Login with</h4>
+            <Button className='mb-2' variant="outline-primary"><FaGofore /> Login with google</Button>
+            <Button variant="outline-secondary"><FaGithub></FaGithub> Login with Github</Button>
+            <div>
+                <h4 className='mt-5'>Find us on</h4>
+                <ListGroup>
+                    <ListGroup.Item><FaFacebook className='ml-2'></FaFacebook>Facebook</ListGroup.Item>
+                    <ListGroup.Item><FaTwitter></FaTwitter>Twitter</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram></FaInstagram>Twitter</ListGroup.Item>
+                </ListGroup>
+            </div>
+            <Qzon></Qzon>
+            <div>
+                <img src={bg} alt="" />
+            </div>
         </div>
+
     );
 };
 
